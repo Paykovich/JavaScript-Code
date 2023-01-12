8905f3b11d240d2596977a010d25519b9bea6767
Paykovich <pavlohazdyk7@gmail.com>
2023-01-12 17:18:01 +0100

    .\homework\

--- homework/object/auto.js
+++ homework/object/auto.js
@@ -5,17 +5,17 @@ const auto = {
     "average speed": 100
 }
 
-const displayInformation = () => console.log(auto)
+const displayInformation = () => console.log(auto);
 
-displayInformation()
+displayInformation();
 
 const timeToOvercome = (distance) => {
-    let time = distance / auto["average speed"]
-    console.log(time)
+    let time = distance / auto["average speed"];
+    console.log(time);
     if (time % 4 === 0) {
         return time + Math.floor((time - 1) / 4);
       }
     return time + Math.floor(time / 4);
 }
 
-timeToOvercome(835)
+timeToOvercome(835);
