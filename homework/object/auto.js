8a9146d34e558bfb2903b5d52aef8cc262bfa633
Paykovich <pavlohazdyk7@gmail.com>
2023-01-12 16:05:35 +0100

    .\auto.js

---
+++ homework/object/auto.js
@@ -0,0 +1,21 @@
+const auto = {
+    manufacturer: "Škoda",
+    model: "Octavia",
+    year: 2019,
+    "average speed": 100
+}
+
+const displayInformation = () => console.log(auto)
+
+displayInformation()
+
+const timeToOvercome = (distance) => {
+    let time = distance / auto["average speed"]
+    console.log(time)
+    if (time % 4 === 0) {
+        return time + Math.floor((time - 1) / 4);
+      }
+    return time + Math.floor(time / 4);
+}
+
+timeToOvercome(835)
