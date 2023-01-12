8d27e665b9fd8c71511375f90f98fb6d09af92ac
Paykovich <pavlohazdyk7@gmail.com>
2023-01-12 17:16:16 +0100

    .\time.js

---
+++ homework/object/time.js
@@ -0,0 +1,44 @@
+const  time = {
+    hours: 24,
+    minutes: 25,
+    seconds: 20
+};
+
+const changingSeconds = (second) => {
+    if (second + time.seconds >= 60) {
+        let inMin = Math.floor((time.seconds + second) / 60);
+        time.seconds = (time.seconds + second) % 60;
+        changingMinutes(inMin);
+    } 
+    else {
+        time.seconds += second;
+    }
+  }
+const changingMinutes = (minute) => {
+    if (time.minutes + minute >= 60) {
+        let InHour = Math.floor((time.minutes + minute) / 60);
+        time.minutes = (time.minutes + minute) % 60;
+        changinHours(InHour);
+    } 
+    else {
+        time.minutes += minute;
+    }
+  }
+const changinHours = (hour) => {
+    if (hour + time.hours >= 24) {
+        time.hours = (time.hours + hour) % 24;
+    } 
+    else {
+        time.hours += hour;
+    }
+}
+
+changingSeconds(105)
+changingMinutes(25)
+changinHours(2)
+
+const timeDisplay = () => {
+    console.log(time.hours + ":" + time.minutes + ":" + time.seconds);
+}
+
+timeDisplay()
