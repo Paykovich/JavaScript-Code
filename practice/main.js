aadc20f40ed9612361b91f5e2b534d79406c735f
Paykovich <pavlohazdyk7@gmail.com>
2023-01-26 18:07:45 +0100

    .\practice\

--- practice/main.js
+++ practice/main.js
@@ -1,9 +1,18 @@
-let btns = document.getElementsByClassName("btn");
+let btns = document.querySelectorAll(".btn");
+let halloween = document.querySelectorAll(".halloween")
+let audio = document.querySelector("audio");
 
-for (let i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click",function(){
+audio.volume = 0.1;
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
         let buttonStyle = getComputedStyle(this);
         let buttonBgColor = buttonStyle["backgroundColor"];
         document.body.style.backgroundColor = buttonBgColor;
+        
     });
+}
+
+function newPageHalloween() {
+    window.location.href = "halloween.html"
 }
