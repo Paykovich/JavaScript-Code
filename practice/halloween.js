d5852f4bbd4bed6e6026fea9b158428886c4df71
Paykovich <pavlohazdyk7@gmail.com>
2023-01-26 18:09:53 +0100

    .\practice\

---
+++ practice/halloween.js
@@ -0,0 +1,3 @@
+let audio = document.querySelector("audio");
+
+audio.volume = 0.1;
--- practice/main.js
+++ practice/main.js
@@ -1,8 +1,5 @@
 let btns = document.querySelectorAll(".btn");
 let halloween = document.querySelectorAll(".halloween")
-let audio = document.querySelector("audio");
-
-audio.volume = 0.1;
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
